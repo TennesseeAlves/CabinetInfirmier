e622027c954f3b176740429d46966f76e416c46b
barkoko <barkokomar@gmail.com>
2025-12-10 10:38:51 +0100

    en cours de dev validation automatique en CS

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿namespace cabinetInfirmier;
+
+using System.Xml;
+
+public class Program {
+    public static void Main(string[] args)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.Load("data/xml/fichePatient.xml");
+        XmlNode root = doc.DocumentElement;
+        // Add namespace
+        XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+        nsmgr.AddNamespace("" ,"http://www.univ-grenoble-alpes.fr/l3miage/patient") ;
+
+        XmlNode nomNode = ((XmlElement)root).GetElementsByTagName("nom").Item(1); // selectionne le noeud
+        String nomString = nomNode.InnerText;
+        Console.WriteLine(nomString);
+        Console.WriteLine(nomNode.Name);
+        Console.WriteLine(nomNode.NodeType);
+    }
+}
