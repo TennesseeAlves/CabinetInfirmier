d9b058f75f676bd4b2b0b8d7e29fcd4c33b196c4
barkoko <barkokomar@gmail.com>
2025-12-11 21:55:28 +0100

    minor refactor Program.cs

--- Program.cs
+++ Program.cs
@@ -6,31 +6,24 @@ class Program
 {
     public static async Task Main(string[] args)
     {
-        //Validation du fichier XML Cabinet
-        //Console.WriteLine("Validation fichiers Cabinet");
-        //XMLUtils.ValidateXmlFileAsync("http://www.univ-grenoble-alpes.fr/l3miage/medical", "../../../data/xsd/cabinet.xsd", "../../../data/xml/cabinet.xml" );
+        // Validation du fichier XML Cabinet
+        Console.WriteLine("Validation fichiers Cabinet");
+        XMLUtils.ValidateXmlFileAsync("http://www.univ-grenoble-alpes.fr/l3miage/medical", "../../../data/xsd/cabinet.xsd", "../../../data/xml/cabinet.xml" );
         
-        //Xslt transformation : 
-        //Console.Write("Transformation Page Infirmere");
-        //XMLUtils.XslTransform2("../../../data/xml/cabinet.xml", "../../../data/xslt/pageInfirmiere.xsl", "../../../data/html/pageInfirmiere.html");
+        // Xslt transformation : 
+        Console.Write("Transformation Page Infirmere");
+        XMLUtils.XslTransform2("../../../data/xml/cabinet.xml", "../../../data/xslt/pageInfirmiere.xsl", "../../../data/html/pageInfirmiere.html");
 
-        
-        //Partie parseur XmlReader :
-        
-        //Cabinet.AnalyseGlobale("./data/xml/cabinet.xml");
+        // Partie parseur XmlReader :
+        Cabinet.AnalyseGlobale("./data/xml/cabinet.xml");
+        Cabinet.AnalyseNomsInfirmiers("./data/xml/cabinet.xml");
+        Cabinet.AnalyseNoms("./data/xml/cabinet.xml", "nom");
 
-        //Cabinet.AnalyseNomsInfirmiers("./data/xml/cabinet.xml");
-        //Cabinet.AnalyseNoms("./data/xml/cabinet.xml", "nom");
+        // Compte le nombre d'actes differents necessaires, tout patient confondus.
+        int actes = Cabinet.countActes("./data/xml/cabinet.xml");
 
-        //Compte le nombre d'actes differents necessaires, tout patient confondus.
-        //int actes = Cabinet.countActes("./data/xml/cabinet.xml");
-
-
-        //Partie parseur DOM :
-
-        
+        // Partie parseur DOM :
         string filename = "./data/xml/cabinet.xml";
-            
         CabinetDOM cab = new CabinetDOM(filename);
        
         /*
@@ -55,14 +48,13 @@ class Program
         Console.WriteLine("-> Adresse du patient '{0}' complète ?? : {1}", nomTestPatient, resPatient);
         */
         
-        //test Partie modification de l'arbre
+        // test Partie modification de l'arbre
         
-        //On ne peut pas ajouter un nouvel infirmier et un patient à la fois (soit l'un soit l'autre), une exception est levée sinon... 
-        
+        // On ne peut pas ajouter un nouvel infirmier et un patient à la fois (soit l'un soit l'autre), une exception est levée sinon... 
         cab.addInfirmier("Némard", "Jean");
         Console.WriteLine("Nouveau infirmier ajouté");
 
-/*
+        /*
         Adresse newAdresse = new Adresse(5, 6, "rue de la paix", "38100", "Chicagre");
         cab.addPatient("KIKS", "Burhan", "2000-03-04", "102039999988876", newAdresse);
         Console.WriteLine("Nouveau patient ajouté");
@@ -73,13 +65,11 @@ class Program
         listActeId.Add(103);
         cab.addVisite("2026-01-04", 003, listActeId, "KIKS");
         Console.WriteLine("Ajout de visite de KIKS");
-*/
+        */
 
         // TEST methode nssValide(nomPatient) qui verifie que le numero de securite social de nomPatient est valide par rapport aux informations
-
         bool resultNSS = cab.nssValide("BARKOK");
         Console.WriteLine("Test Omar: {0}", resultNSS);
-        
         
         // Partie serialisation 
         
@@ -101,8 +91,8 @@ class Program
         var infirManager = new XMLManager<Infirmier>();
         var TestInfirmier = new Infirmier("005", "BARKOK", "Omar", "Omar.png");
         string pathInfir = "../../../data/perso/infirmier.xml";
-        //XmlSerializerNamespaces nsInfir =  new XmlSerializerNamespaces();
-        //nsInfir.Add("cab", "http://www.univ-grenoble-alpes.fr/l3miage/medical/Inf");
+        // XmlSerializerNamespaces nsInfir =  new XmlSerializerNamespaces();
+        // nsInfir.Add("cab", "http://www.univ-grenoble-alpes.fr/l3miage/medical/Inf");
         infirManager.Save(pathInfir, TestInfirmier);
         Console.WriteLine("Serealisation de Infirmier effectué");
         
@@ -115,45 +105,42 @@ class Program
         Console.WriteLine("Serealisation de Infirmier effectué");
         
         
-        //Acte
+        // Acte
         XMLManager<Acte> acteManager = new XMLManager<Acte>();
         Acte acteSerealisation = new Acte("503");
         acteManager.Save("../../../data/perso/acte.xml",  acteSerealisation);
         
-        //Visite 
+        // Visite 
         List<Acte> listActes = new List<Acte>();
         listActes.Add(acteSerealisation);
         XMLManager<Visite>  visiteManager = new XMLManager<Visite>();
         Visite visiteSereal = new Visite("005", "2025-01-23", listActes);
         visiteManager.Save("../../../data/perso/visite.xml", visiteSereal);
         
-        //Patient
+        // Patient
         XMLManager<Patient>  patientManager = new XMLManager<Patient>();
         Patient patientSereal = new Patient("Orouge", "Elvire", "F", "1982-03-08", "282036912305243", TestAdresse, visiteSereal);
         patientManager.Save("../../../data/perso/patient.xml", patientSereal);
         
-        //Patients
+        // Patients
         XMLManager<Patients> patientsManager = new XMLManager<Patients>();
         List<Patient> patientsList = new List<Patient>();
         patientsList.Add(patientSereal);
         Patients patientsSeral = new Patients(patientsList);
         patientsManager.Save("../../../data/perso/patients.xml", patientsSeral);
         
-        
-        //Cabinet
+        // Cabinet
         XMLManager<Cabinet> cabinetManager = new XMLManager<Cabinet>();
         Cabinet cabientSereal = new  Cabinet("Mon Cabinet", TestAdresse, infirmiersSer, patientsSeral);
         cabinetManager.Save("../../../data/perso/cabinet_modif.xml",cabientSereal);
         
         */
-        
-        //Cabinet deserealisation :
-        //Partie 7.4.4 ajout d'un patient au cabinet
-        
-        /*XMLManager<Cabinet> cabinetDeserealManager = new XMLManager<Cabinet>();
+        // Cabinet deserealisation :
+        // Partie 7.4.4 ajout d'un patient au cabinet
+        /*
+        XMLManager<Cabinet> cabinetDeserealManager = new XMLManager<Cabinet>();
         Cabinet cabinetDesereal = cabinetDeserealManager.Load("../../../data/xml/cabinet.xml");
         Console.WriteLine(cabinetDesereal.toString());
-        
         
         Acte newActe = new Acte("105");
         List<Acte> newListActe = new List<Acte>();
@@ -165,7 +152,8 @@ class Program
         
         cabinetDesereal.Patients.addPatient(newPatient);
         
-        cabinetDeserealManager.Save("../../../data/xml/cabinet_modif.xml", cabinetDesereal);*/
+        cabinetDeserealManager.Save("../../../data/xml/cabinet_modif.xml", cabinetDesereal);
+        */
         
     }
 }
